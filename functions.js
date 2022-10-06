84277327048bca3b8301458fd32810d418ab9865
Sinesy <contato.sinesy@gmail.com>
2022-10-06 15:28:48 -0300

    first commit

---
+++ functions.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const fileToJSON = (filePath) => {
+  return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+};
+
+const jsonToFile = (filePath, json) => {
+  fs.writeFileSync(filePath, JSON.stringify(json));
+};
+
+const rootPath = (fileName) => {
+  return path.join(__dirname, fileName);
+};
+
+module.exports = { fileToJSON, jsonToFile, rootPath };
