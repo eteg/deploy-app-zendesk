7932943fc90841e8f809a68375ed744bbfa56804
Rodrigo Moura <rodrigovianasos@gmail.com>
2022-11-10 15:51:42 -0300

    debuggin steps

--- src/createApp.ts
+++ src/createApp.ts
@@ -9,19 +9,36 @@ export async function createApp(
   distPath: string
 ): Promise<AppId> {
 
+  console.log({ authenticate });
+  console.log({ parameters });
+  console.log({ appConfig });
+  console.log({ distPath });
+
   const { api } = new ZendeskAuthentication(authenticate);
+
+  console.log({ api });
 
   const commonApp = new CommonApp(api);
 
   const { id: newAppUploadId } = await commonApp.uploadApp(distPath);
 
+  console.log({ newAppUploadId });
+
   const appName = appConfig.name;
+
+  console.log({ appName });
 
   const { job_id } = await commonApp.deployApp(newAppUploadId, appName, 'post');
 
+  console.log({ job_id });
+
   const { app_id: appIdFromJobStatus } = await commonApp.getUploadJobStatus(job_id);
 
+  console.log({ appIdFromJobStatus });
+
   const { app_id } = await commonApp.updateProductInstallation(cleanParameters(parameters), appConfig, appIdFromJobStatus);
+
+  console.log({ app_id });
 
   return app_id;
 }
