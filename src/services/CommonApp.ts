472c67fd758dc8c73f5a3bbb3670abcc760dcb74
Vinicius <viniciusmarch01@gmail.com>
2022-12-01 15:11:24 -0300

    test

--- src/services/CommonApp.ts
+++ src/services/CommonApp.ts
@@ -37,21 +37,33 @@ export default class CommonApp {
     //   }
     // };
 
-    const { data } = await this._apiAuthentication.post("api/v2/apps/uploads.json", form);
+    const { data } = await this._apiAuthentication.post(
+      "api/v2/apps/uploads.json",
+      form
+    );
 
     console.log({ data });
 
     return data;
   }
 
-  async deployApp(uploadId: string, name: string, httpMethod: "post" | "put"): Promise<{ job_id: string }> {
-    const payload: { upload_id: string; name?: string } = { upload_id: uploadId };
+  async deployApp(
+    uploadId: string,
+    name: string,
+    httpMethod: "post" | "put"
+  ): Promise<{ job_id: string }> {
+    const payload: { upload_id: string; name?: string } = {
+      upload_id: uploadId,
+    };
     if (name) {
       payload.name = name;
     }
     console.log("payload", payload);
 
-    const { data } = await this._apiAuthentication[httpMethod]("api/v2/apps.json", payload);
+    const { data } = await this._apiAuthentication[httpMethod](
+      "api/v2/apps.json",
+      payload
+    );
     console.log("data", data);
 
     return data;
@@ -61,11 +73,17 @@ export default class CommonApp {
   async getUploadJobStatus(job_id: string, pollAfter = 1000): Promise<any> {
     return new Promise((resolve, reject) => {
       const polling = setInterval(async () => {
-        const { data } = await this._apiAuthentication.get(`api/v2/apps/job_statuses/${job_id}`);
+        const { data } = await this._apiAuthentication.get(
+          `api/v2/apps/job_statuses/${job_id}`
+        );
 
         if (data.status === "completed") {
           clearInterval(polling);
-          resolve({ status: data.status, message: data.message, app_id: data.app_id });
+          resolve({
+            status: data.status,
+            message: data.message,
+            app_id: data.app_id,
+          });
         } else if (data.status === "failed") {
           clearInterval(polling);
           reject(data.message);
@@ -81,23 +99,36 @@ export default class CommonApp {
   ): Promise<{ app_id: string }> {
     //TODO: Verificar se o app_id está certo
 
+    console.log({ app_id, settings: parameters }, "aq1uuui");
+
     await this._apiAuthentication.post(`/api/support/apps/installations.json`, {
       app_id,
       settings: parameters,
     });
 
-    const installationResp = await this._apiAuthentication.get(`/api/support/apps/installations.json`);
+    const installationResp = await this._apiAuthentication.get(
+      `/api/support/apps/installations.json`
+    );
     console.log(installationResp, "installationResp TODAS AS INSTALAÇÕES");
 
     const { installations } = installationResp.data;
-    console.log("LINGUICETA", JSON.stringify(installations, null, 2), "LINGUICETA");
+    console.log(
+      "LINGUICETA",
+      JSON.stringify(installations, null, 2),
+      "LINGUICETA"
+    );
 
-    const installation_id = installations.find((i: Installation) => String(i.app_id) === String(app_id))?.id;
+    const installation_id = installations.find(
+      (i: Installation) => String(i.app_id) === String(app_id)
+    )?.id;
     console.log({ installation_id }, "achou algo?");
 
-    const { data } = await this._apiAuthentication.put(`/api/support/apps/installations/${installation_id}.json`, {
-      settings: { name: manifest.name, ...parameters },
-    });
+    const { data } = await this._apiAuthentication.put(
+      `/api/support/apps/installations/${installation_id}.json`,
+      {
+        settings: { name: manifest.name, ...parameters },
+      }
+    );
 
     return data;
   }
