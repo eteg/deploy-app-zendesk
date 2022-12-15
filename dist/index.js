7b456784eb3bda207dc1e43c6fcd629fe46b8187
miacelPereira <miacelPereira@users.noreply.github.com>
2022-12-15 19:48:13 +0000

    build script with git-auto-commit

--- dist/index.js
+++ dist/index.js
@@ -25584,9 +25584,7 @@ class CommonApp {
             const form = new form_data_1.default();
             form.append("uploaded_data", fs_1.default.createReadStream(outputFile));
             const retorno = yield this._apiAuthentication.post("api/v2/apps/uploads.json", form, {
-                headers: {
-                    "Content-type": "application/zip"
-                }
+                headers: Object.assign({}, form.getHeaders())
             });
             return retorno.data;
         });
