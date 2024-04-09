71e0b92cecc0e15a9a1380b40acc5a01b4b73d7a
KayanHendges <kayanbhendges@gmail.com>
2024-04-09 12:13:24 -0300

    test

--- dist/index.js
+++ dist/index.js
@@ -23065,7 +23065,6 @@ function getAppInput() {
     const roleRestrictions = roleRestrictionsInput
         ? (0, string_1.stringToArrayOfIds)(roleRestrictionsInput)
         : undefined;
-    console.log({ roleRestrictions });
     if (appPath && appPackage) {
         throw new Error("Parameters validation: You can't fill both 'path' and 'package' parameters.");
     }
